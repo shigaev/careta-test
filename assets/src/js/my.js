6b32f2a90a2b656dcdb222650f677855d286f330
ShigaevDV <ShigaevDV@gup.ru>
2019-10-22 15:34:43 +0300

    first commit

---
+++ assets/src/js/my.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel();
+});
+
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    nav: true,
+});
+$('.play').on('click', function() {
+    owl.trigger('play.owl.autoplay', [1000])
+})
+$('.stop').on('click', function() {
+    owl.trigger('stop.owl.autoplay')
+})
